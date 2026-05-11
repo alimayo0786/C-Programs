928c5d88c40d11b0184c8403f37eb794ed661fef
Ali Mayo <alikhanmayo00786@gmail.com>
2026-05-11 10:31:21 +0500

    in ko bi waha store kr do

---
+++ 11_5_2026/total salay.cpp
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int basic,bonus,ans;
+	printf("Enter 1st number\n");
+	scanf("%d",&basic);
+	printf("Enter 2nd number\n");
+	scanf("%d",&bonus);
+	ans=basic+bonus;
+	printf("%d %d %d",basic,bonus,ans);
+}
+	
